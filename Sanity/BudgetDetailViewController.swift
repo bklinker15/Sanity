969af79b5be6d32a72164a6eccc409385cab0747
Jordan Coppert <jordancoppert@gmail.com>
2017-10-15 12:03:55 -0700

    Updated DashboardVC to transition to detailVC with correct data pulled from the tapped cell. Also populated the UI for adding a transaction, still needs to be hooked up.

---
+++ Sanity/BudgetDetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  BudgetDetailViewController.swift
+//  Sanity
+//
+//  Created by Jordan Coppert on 10/15/17.
+//  Copyright © 2017 CSC310Team22. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class BudgetDetailViewController: UIViewController {
+    var budgetName:String?
+    var categories = [Category]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
