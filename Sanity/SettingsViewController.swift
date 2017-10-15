40a81cf32eddaf33b0a7efee969265b959d51cdd
Nick Kaimakis <kaimakis@usc.edu>
2017-10-15 09:47:29 -0700

    attempt to fix file not found error

--- Sanity/SettingsViewController.swift
+++ Sanity/SettingsViewController.swift
@@ -14,6 +14,7 @@ class SettingsViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
     }
 
     override func didReceiveMemoryWarning() {
