e9bf1d4f6d5ad6511adf9aec9be52a2819c17b68
Nick Kaimakis <kaimakis@usc.edu>
2017-10-15 20:47:21 -0700

    picker view updates

--- Sanity/SettingsViewController.swift
+++ Sanity/SettingsViewController.swift
@@ -14,7 +14,7 @@ class SettingsViewController: UIViewController {
     var userEmail: String?
     var notificationSettingsIndex: Int = 0
     
-    var notificationSettings = ["budget exceeded and threshold exceeded","budget exceeded only","none"]
+    var pickerData = ["budget exceeded and threshold exceeded","budget exceeded only","none"]
     
     @IBOutlet weak var notificationsPicker: UIPickerView!
     
@@ -67,6 +67,9 @@ class SettingsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Connect data:
+        self.notificationsPicker.delegate = self as? UIPickerViewDelegate
+        self.notificationsPicker.dataSource = self as? UIPickerViewDataSource
         //        var index: Int = getNotificationsIndex()
         //TODO: set picker view to correct index
         
@@ -77,6 +80,21 @@ class SettingsViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // The number of columns of data
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // The number of rows of data
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    // The data to return for the row and component (column) that's being passed in
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
     }
     
     
