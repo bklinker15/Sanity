dfa9081ac2d781eb351224da15a00a3d3df62dc8
Katie Wasserman <katiewasserman@Katies-MacBook-Pro.local>
2017-10-15 20:34:42 -0700

    budget name

--- Sanity/BudgetDetailViewController.swift
+++ Sanity/BudgetDetailViewController.swift
@@ -24,6 +24,8 @@ class BudgetDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        budgetNm.text = budgetName;
     }
 
     override func didReceiveMemoryWarning() {
