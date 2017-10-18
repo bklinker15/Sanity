3327ee08cd4f536ad982e0526e1ba4132fc3fa9d
Katie Wasserman <katiewasserman@Katies-MacBook-Pro.local>
2017-10-18 13:28:53 -0700

    commit

---
+++ Sanity/EditBudgetViewController.swift
@@ -0,0 +1,28 @@
+//
+//  EditBudgetViewController.swift
+//  Sanity
+//
+//  Created by Katie Wasserman on 18/10/17.
+//  Copyright © 2017 CSC310Team22. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class EditBudgetViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+
+    
+    // delete budget button
+    // edit categories (add or delete)
+    // edit budget period
+    
+    
+}
